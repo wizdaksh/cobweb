6671775b6fd41903a9cd0f43eebafc6b701146fd
Daksh Khandelwal <slay3rxo@gmail.com>
2024-04-06 00:07:17 -0400

    Starting of page content

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IrRFNNWL+rDNhnuoAqcbWk6qQtl66FLFcVtiLnDSWQI=\"\n}"
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
+import "./home.css";
+
 export default function Home() {
   return (
     <div className="page-content hero">
-      <h1>
-        The Nest<sup>(01)</sup>
-      </h1>
-      <p>Grow your business</p>
+      <container className="landing-hook">
+        <h2 className="tracking-in-expand-fwd-top">We are Reweb Studio</h2>
+        <p className="tracking-in-expand-fwd-bottom" id="grow">Digitizing Your Creativity</p>
+      </container>
     </div>
   );
 }
